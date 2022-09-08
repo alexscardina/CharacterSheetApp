f3ae4f809db71ed580dcd5791a9f19e4c279503a
Alex Scardina <Alex@Alexs-MacBook-Pro-2.local>
2022-09-07 21:40:29 -0400

    Added components

---
+++ app/src/Components/CharacterInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Components.css';
+
+function CharacterInfo() {
+    return(
+        <div>
+            <div class="info-card name-card">
+                Character Name here
+            </div>
+            <div class="info-card class-card">
+                Class information here
+            </div>
+        </div>
+    )
+}
+
+export default CharacterInfo;
